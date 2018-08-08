8bdc35db1f081d4888e06bc114bbc4ec00c4df6d
Jinseo <rinndash@gmail.com>
2018-08-08 22:47:36 +0900

    현재 날짜를 DateLabel에 표시

--- Journal/Sources/Controllers/EntryViewController.swift
+++ Journal/Sources/Controllers/EntryViewController.swift
@@ -8,13 +8,23 @@
 
 import UIKit
 
+extension DateFormatter {
+    static var entryDateFormatter: DateFormatter = {
+        let df = DateFormatter.init()
+        df.dateFormat = "yyyy. M. dd. EEE"
+        return df
+    }()
+}
+
 class EntryViewController: UIViewController {
     @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var dateLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         textView.text = "첫 번째 일기"
+        dateLabel.text = DateFormatter.entryDateFormatter.string(from: Date())
     }
 }
 
