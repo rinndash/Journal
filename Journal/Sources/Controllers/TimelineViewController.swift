c8ddb0b632d3bfb42c4a2b968641c7b8a228d518
Jinseo <rinndash@gmail.com>
2018-08-11 01:14:55 +0900

    TimelineViewController를 추가

---
+++ Journal/Sources/Controllers/TimelineViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TimelineViewController.swift
+//  Journal
+//
+//  Created by 윤진서 on 2018. 8. 11..
+//  Copyright © 2018년 Jinseo Yoon. All rights reserved.
+//
+
+import UIKit
+
+class TimelineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Journal"
+    }
+}
