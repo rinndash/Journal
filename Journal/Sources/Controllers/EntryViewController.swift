21553e69f1ae40e40b12aa1dceb0164ed46d5576
Jinseo <rinndash@gmail.com>
2018-08-10 21:47:00 +0900

    더미 텍스트 추가

--- Journal/Sources/Controllers/EntryViewController.swift
+++ Journal/Sources/Controllers/EntryViewController.swift
@@ -16,6 +16,62 @@ extension DateFormatter {
     }()
 }
 
+private let loremIpsum = """
+class EntryViewController: UIViewController {
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var button: UIButton!
+
+    private let journal: Journal = InMemoryJournal()
+    private var editingEntry: Entry?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        textView.text = loremIpsum
+        dateLabel.text = DateFormatter.entryDateFormatter.string(from: Date())
+
+        updateSubviews(for: true)
+    }
+
+    @objc private func saveEntry() {
+        if let oldEntry = self.editingEntry {
+            oldEntry.text = textView.text
+            journal.update(oldEntry)
+        } else {
+            let newEntry: Entry = Entry(text: textView.text)
+            journal.add(newEntry)
+            editingEntry = newEntry
+        }
+
+        updateSubviews(for: false)
+    }
+
+    @objc private func editEntry() {
+        updateSubviews(for: true)
+    }
+
+    private func updateSubviews(for isEditing: Bool) {
+        if isEditing {
+            textView.isUserInteractionEnabled = true
+            textView.becomeFirstResponder()
+
+            button.setTitle("저장하기", for: .normal)
+            button.removeTarget(self, action: nil, for: .touchUpInside)
+            button.addTarget(self, action: #selector(saveEntry), for: .touchUpInside)
+
+        } else {
+            textView.isUserInteractionEnabled = false
+            textView.resignFirstResponder()
+
+            button.setTitle("수정하기", for: .normal)
+            button.removeTarget(self, action: nil, for: .touchUpInside)
+            button.addTarget(self, action: #selector(editEntry), for: .touchUpInside)
+        }
+    }
+}
+"""
+
 class EntryViewController: UIViewController {
     @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var dateLabel: UILabel!
@@ -27,7 +83,7 @@ class EntryViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        textView.text = "첫 번째 일기"
+        textView.text = loremIpsum
         dateLabel.text = DateFormatter.entryDateFormatter.string(from: Date())
         
         updateSubviews(for: true)
