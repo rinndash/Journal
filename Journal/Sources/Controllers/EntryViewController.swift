8a60d027254920796be889a1c42ba384495aa2e0
Jinseo Yoon <rinndash@gmail.com>
2018-08-11 12:35:14 +0900

    수정 기능 추가

--- Journal/Sources/Controllers/EntryViewController.swift
+++ Journal/Sources/Controllers/EntryViewController.swift
@@ -19,21 +19,64 @@ extension DateFormatter {
 class EntryViewController: UIViewController {
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var button: UIButton!
     
     let journal: Journal = InMemoryJournal()
+    private var editingEntry: Entry?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         dateLabel.text = DateFormatter.entryDateFormatter.string(from: Date())
         textView.text = "IBOutlet으로 연결한 TextView"
-    }
-
-    @IBAction func saveEntry(_ sender: Any) {
-        let entry: Entry = Entry(text: textView.text)
-        journal.add(entry)
         
-        print("Entry 개수: ", journal.numberOfEntries)
+        button.addTarget(self, 
+                         action: #selector(saveEntry(_:)), 
+                         for: .touchUpInside
+        )
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        textView.becomeFirstResponder()
+    }
+    
+    @objc func saveEntry(_ sender: Any) {
+        if let editing = editingEntry {
+            editing.text = textView.text
+            journal.update(editing)
+        } else {
+            let entry: Entry = Entry(text: textView.text)
+            journal.add(entry)
+            editingEntry = entry
+        }
+        
+        updateSubviews(for: false)
+    }
+    
+    @objc func editEntry(_ sender: Any) {
+        updateSubviews(for: true)
+    }
+    
+    fileprivate func updateSubviews(for isEditing: Bool) {
+        if isEditing {
+            textView.isEditable = true
+            textView.becomeFirstResponder()
+            
+            button.setTitle("저장하기", for: .normal)
+            button.addTarget(self, 
+                             action: #selector(saveEntry(_:)), 
+                             for: .touchUpInside)
+        } else {
+            textView.isEditable = false
+            textView.resignFirstResponder()
+            
+            button.setTitle("수정하기", for: .normal)
+            button.addTarget(self, 
+                             action: #selector(editEntry(_:)), 
+                             for: .touchUpInside)
+        }        
     }
 }
 
