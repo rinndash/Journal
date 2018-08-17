721c6a85d6f6672c16eaa3ffb73d6a682c7e1f56
Jinseo <rinndash@gmail.com>
2018-08-18 01:14:00 +0900

    테스트 케이스 작성 완료

--- JournalTests/EntryViewViewModelTests.swift
+++ JournalTests/EntryViewViewModelTests.swift
@@ -10,6 +10,34 @@ import XCTest
 import Nimble
 @testable import Journal
 
+class MockEntryRepository: EntryRepository {
+    var numberOfEntries: Int { return 0 }
+    
+    var addedEntry: Entry?
+    var updatedEntry: Entry?
+    var removedEntry: Entry?
+    
+    func add(_ entry: Entry) {
+        addedEntry = entry
+    }
+    
+    func update(_ entry: Entry) {
+        updatedEntry = entry
+    }
+    
+    func remove(_ entry: Entry) {
+        removedEntry = entry
+    }
+    
+    func entry(with id: UUID) -> Entry? {
+        return nil
+    }
+    
+    func recentEntries(max: Int) -> [Entry] {
+        return []
+    }
+}
+
 class EntryViewViewModelTests: XCTestCase {
     func testHasEntry() {
         // Setup
@@ -113,5 +141,50 @@ class EntryViewViewModelTests: XCTestCase {
         expect(viewModel.isEditing) == false
         expect(viewModel.textViewEditable) == false
         expect(viewModel.buttonImage) == #imageLiteral(resourceName: "baseline_edit_white_24pt")
+    }
+    
+    func testAddEntryToRepositoryWhenEntryPropertyIsNil() {
+        let mockRepo = MockEntryRepository()
+        let environment = Environment(entryRepository: mockRepo)
+        let viewModel = EntryViewControllerModel(environment: environment)
+        
+        // Run
+        viewModel.completeEditing(with: "첫 번째 생성")
+        
+        // Verify
+        expect(mockRepo.addedEntry?.text) == "첫 번째 생성"
+        expect(mockRepo.updatedEntry).to(beNil())
+        expect(viewModel.hasEntry) == true
+    }
+    
+    func testUpdateEntryInRepositoryWhenEntryExists() {
+        let mockRepo = MockEntryRepository()
+        let environment = Environment(entryRepository: mockRepo)
+        let entry = Entry(text: "처음 작성한 일기")
+        let viewModel = EntryViewControllerModel(environment: environment, entry: entry)
+        
+        // Run
+        viewModel.completeEditing(with: "첫 번째 수정")
+        
+        // Verify
+        expect(mockRepo.updatedEntry?.text) == "첫 번째 수정"
+        expect(mockRepo.updatedEntry?.isIdentical(to: entry)).to(beTrue())
+        expect(mockRepo.addedEntry).to(beNil())
+    }
+    
+    func testRemoveEntry() {
+        // Setup
+        let mockRepo = MockEntryRepository()
+        let environment = Environment(entryRepository: mockRepo)
+        let entry = Entry(text: "제거할 일기")
+        let viewModel = EntryViewControllerModel(environment: environment, entry: entry)
+        
+        // Run
+        let removedEntry = viewModel.removeEntry()
+        
+        // Verify
+        expect(removedEntry) == entry
+        expect(mockRepo.removedEntry) == entry
+        expect(viewModel.hasEntry) == false
     }
 }
