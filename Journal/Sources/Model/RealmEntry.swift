a9b1d9780928b7e1be651fa1cb6569c63d357266
Jinseo <rinndash@gmail.com>
2018-08-30 05:00:07 +0900

    RealmEntry를 추가

---
+++ Journal/Sources/Model/RealmEntry.swift
@@ -0,0 +1,25 @@
+//
+//  RealmEntry.swift
+//  Journal
+//
+//  Created by 윤진서 on 2018. 8. 30..
+//  Copyright © 2018년 Jinseo Yoon. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmEntry: Object, EntryType {
+    var id: UUID {
+        guard let uuid = UUID.init(uuidString: uuidString) else { fatalError() }
+        return uuid
+    }
+    
+    @objc dynamic var uuidString: String = ""
+    @objc dynamic var createdAt: Date = Date()
+    @objc dynamic var text: String = ""
+    
+    override static func primaryKey() -> String? {
+        return "uuidString"
+    }
+}
